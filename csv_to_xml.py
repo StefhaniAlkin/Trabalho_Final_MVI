024b123387b8b33170ccdc43d9a2386e98a77fad
Stefhani Alkin <90950170+StefhaniAlkin@users.noreply.github.com>
2023-12-05 18:02:30 -0300

    :white_small_square: feat: conversão de csv para xml

---
+++ csv_to_xml.py
@@ -0,0 +1,22 @@
+import csv
+import xml.etree.ElementTree as ET
+
+def csv_to_xml(csv_file, xml_file):
+    with open(csv_file, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        root = ET.Element('listaDeProdutos')
+ 
+        for row in csv_reader:
+            produto = ET.SubElement(root, 'produto')
+ 
+            for key, value in row.items():
+                element = ET.SubElement(produto, key)
+                element.text = value
+ 
+        tree = ET.ElementTree(root)
+        tree.write(xml_file, encoding='utf-8')
+        
+csv_filename = "entrada.csv"
+xml_filename = "saida.xml"
+    
+csv_to_xml(csv_filename, xml_filename)
