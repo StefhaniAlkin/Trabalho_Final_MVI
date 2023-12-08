9e39157b2e5771f700cc898a3c61fc4bb5514f15
Stefhani Alkin <90950170+StefhaniAlkin@users.noreply.github.com>
2023-12-08 16:11:56 -0300

    :white_small_square: fix: removido a conversão de PDF (ainda não implementado)

--- gerador_nf.py
+++ gerador_nf.py
@@ -5,7 +5,6 @@ from csv_to_yaml import *
 
 parser = argparse.ArgumentParser("Projeto Final: Gerador/conversor de notas fiscais")
 parser.add_argument("--formato", dest='formato', help="O formato desejado da nota fiscal. Opções disponíveis: XML, JSON e YAML.", type=str, choices=['xml', 'json', 'yaml'])
-parser.add_argument("--pdf", dest='pdf', help="Indica se um pdf deve ser gerado ou não", type=bool, default=False)
 args = parser.parse_args()
 
 if args.formato == 'json':
