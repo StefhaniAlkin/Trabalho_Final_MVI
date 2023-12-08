5c342f34ba70c26270424ca02b821139cd5e0938
Stefhani Alkin <90950170+StefhaniAlkin@users.noreply.github.com>
2023-12-08 15:30:58 -0300

    :white_small_square: feat: converter csv para yaml

---
+++ csv_to_yaml.py
@@ -0,0 +1,17 @@
+import csv
+import yaml
+
+def csv_to_yaml(csv_file, yaml_file):
+    data = []
+    with open(csv_file, 'r', newline='', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+
+    with open(yaml_file, 'w', encoding='utf-8') as yaml_file:
+        yaml.dump(data, yaml_file, indent=2)
+
+csv_filename = "entrada.csv"
+yaml_filename = "saida.yaml"
+
+csv_to_yaml(csv_filename, yaml_filename)
